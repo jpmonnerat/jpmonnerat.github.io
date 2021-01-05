eba94034ca6680ca18607c3d502de6e0c68b8309
João Pedro Monnerat <jpmonnerat@localhost.localdomain>
2021-01-05 19:51:57 -0300

    Enviando paginas prontas, falta apenas a pagina de apoio e finalizar a pagina informativa

--- js/site.js
+++ js/site.js
@@ -1,3 +1,4 @@
+//Funções da página principal
 $("#btn").css('display', 'none');
 
 $("#show").click(function(){
@@ -6,4 +7,104 @@ $("#show").click(function(){
 
 $("#hide").click(function(){
     $("#btn").hide(10);
+});
+
+$(".carousel").slick({
+    infinite: true,
+    centerMode: true,
+    centerPadding: '120px',
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 701,
+            setings: {
+                infinite: true,
+                centerMode: true,
+                centerPadding: '100px',
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                variableWidth: true,
+            }
+        },
+        {
+            breakpoint: 699,
+            setings: {
+                infinite: true,
+                centerMode: true,
+                centerPadding: '80px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: true,
+            }
+        }
+    ]
+});
+
+
+//funções da pág. de bandeiras
+
+$("#flags").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 701,
+            setings: {
+                infinite: true,
+                centerMode: true,
+                centerPadding: '100px',
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                variableWidth: true,
+            }
+        },
+        {
+            breakpoint: 699,
+            setings: {
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: true,
+            }
+        }
+    ]
+});
+
+//Funções da pág. sobre índices de homicídios
+
+$(".carousel-inf").slick({
+    infinite: true,
+    centerMode: true,
+    centerPadding: '120px',
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 701,
+            setings: {
+                infinite: true,
+                centerMode: true,
+                centerPadding: '100px',
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                variableWidth: true,
+            }
+        },
+        {
+            breakpoint: 699,
+            setings: {
+                infinite: true,
+                centerMode: true,
+                centerPadding: '80px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: true,
+            }
+        }
+    ]
 });
